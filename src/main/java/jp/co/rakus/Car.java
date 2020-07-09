c6eb91105922796323fc8281dc7eb71643501fe3
森本 義基 <yoshiki.morimoto@rakus.internal>
2020-07-09 11:34:25 +0900

    フィールド変数の追加

--- src/main/java/jp/co/rakus/Car.java
+++ src/main/java/jp/co/rakus/Car.java
@@ -1,5 +1,25 @@
 package jp.co.rakus;
 
 public class Car {
+	private int speed = 50;
 
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+	
+	private int gas = 30;
+
+	public int getGas() {
+		return gas;
+	}
+
+	public void setGas(int gas) {
+		this.gas = gas;
+	}
+	
+	
 }
