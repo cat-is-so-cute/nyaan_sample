8de56565be0732ad0694ccebe2a6a71b18148b76
cat-is-so-cute <ryosuke.nakanishi@rakus.co.jp>
2020-07-09 11:33:18 +0900

    speed, gas変数を作成

--- src/main/java/jp/co/rakus/Car.java
+++ src/main/java/jp/co/rakus/Car.java
@@ -1,5 +1,24 @@
 package jp.co.rakus;
 
 public class Car {
+	private int speed = 50;
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	private int gas = 60;
+
+	public int getGas() {
+		return gas;
+	}
+
+	public void setGas(int gas) {
+		this.gas = gas;
+	}
 
 }
