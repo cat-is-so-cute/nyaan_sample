0bb17c6e3e6a99823fe24eb2825f51eeedfa3b60
cat-is-so-cute <ryosuke.nakanishi@rakus.co.jp>
2020-07-09 11:06:26 +0900

    first commit

---
+++ src/main/java/jp/co/rakus/Car.java
@@ -0,0 +1,5 @@
+package jp.co.rakus;
+
+public class Car {
+
+}
---
+++ src/main/java/jp/co/rakus/NyaanApplication.java
@@ -0,0 +1,13 @@
+package jp.co.rakus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NyaanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NyaanApplication.class, args);
+	}
+
+}
